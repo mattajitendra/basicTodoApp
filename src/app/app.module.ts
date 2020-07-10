fe2aa3ee888762a73e8cc865dd65178d653b1123
mattajitendra <66953300+mattajitendra@users.noreply.github.com>
2020-07-10 09:14:23 +0530

    Add files via upload
    Basic Todo app to create todo list

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    FontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
